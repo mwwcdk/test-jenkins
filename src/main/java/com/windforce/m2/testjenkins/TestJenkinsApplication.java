a0ec18619860e6e712cda4fb573587d929bfd03a
cdk <lcfcdk@163.com>
2025-03-31 14:45:03 +0800

    新增Jenkinsfile

--- src/main/java/com/windforce/m2/testjenkins/TestJenkinsApplication.java
+++ src/main/java/com/windforce/m2/testjenkins/TestJenkinsApplication.java
@@ -2,12 +2,20 @@ package com.windforce.m2.testjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestJenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestJenkinsApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String hello() {
+        return "Hello World";
     }
 
 }
